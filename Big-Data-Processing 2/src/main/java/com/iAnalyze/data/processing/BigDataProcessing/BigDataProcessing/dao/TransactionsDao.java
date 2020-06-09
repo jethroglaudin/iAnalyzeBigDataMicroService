e210e4cffd6b556fa8bd90774ed75986354c7452
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-06-08 23:18:52 -0400

    updated TransactionDao interface

--- Big-Data-Processing 2/src/main/java/com/iAnalyze/data/processing/BigDataProcessing/BigDataProcessing/dao/TransactionsDao.java
+++ Big-Data-Processing 2/src/main/java/com/iAnalyze/data/processing/BigDataProcessing/BigDataProcessing/dao/TransactionsDao.java
@@ -1,10 +1,12 @@
 package com.iAnalyze.data.processing.BigDataProcessing.BigDataProcessing.dao;
 
+import com.iAnalyze.data.processing.BigDataProcessing.BigDataProcessing.model.AnalyzedTransactions;
 import com.iAnalyze.data.processing.BigDataProcessing.BigDataProcessing.model.Transaction;
 
 import java.util.List;
 
 public interface TransactionsDao {
     public void insert(List<? extends Transaction> transactions);
+    public void analyzeInsert(List<? extends AnalyzedTransactions> analyzedTransactions);
     List<Transaction> loadAllTransactions();
 }
