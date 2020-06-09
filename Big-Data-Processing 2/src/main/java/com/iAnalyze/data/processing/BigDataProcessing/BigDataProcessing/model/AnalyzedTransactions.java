ad1e74d9d1f8e1d17356efb21c0901812550a609
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-06-08 20:43:20 -0400

    created Analyzed Transactions model

---
+++ Big-Data-Processing 2/src/main/java/com/iAnalyze/data/processing/BigDataProcessing/BigDataProcessing/model/AnalyzedTransactions.java
@@ -0,0 +1,43 @@
+package com.iAnalyze.data.processing.BigDataProcessing.BigDataProcessing.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+public class AnalyzedTransactions {
+    private int step;
+    private int totalFraudlentPerStep;
+    private double avgFraudPerStep;
+
+    public AnalyzedTransactions(int step, int totalFraudlentPerStep, double avgFraudPerStep) {
+        this.step = step;
+        this.totalFraudlentPerStep = totalFraudlentPerStep;
+        this.avgFraudPerStep = avgFraudPerStep;
+    }
+
+    public int getStep() {
+        return step;
+    }
+
+    public void setStep(int step) {
+        this.step = step;
+    }
+
+    public int getTotalFraudlentPerStep() {
+        return totalFraudlentPerStep;
+    }
+
+    public void setTotalFraudlentPerStep(int totalFraudlentPerStep) {
+        this.totalFraudlentPerStep = totalFraudlentPerStep;
+    }
+
+    public double getAvgFraudPerStep() {
+        return avgFraudPerStep;
+    }
+
+    public void setAvgFraudPerStep(double avgFraudPerStep) {
+        this.avgFraudPerStep = avgFraudPerStep;
+    }
+}
