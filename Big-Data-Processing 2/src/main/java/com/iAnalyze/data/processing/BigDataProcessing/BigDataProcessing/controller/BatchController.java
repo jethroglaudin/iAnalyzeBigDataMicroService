4b2c2b2676c0534603e5a00f8355909ef4084585
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-07-15 22:19:14 -0400

    refactored

--- Big-Data-Processing 2/src/main/java/com/iAnalyze/data/processing/BigDataProcessing/BigDataProcessing/controller/BatchController.java
+++ Big-Data-Processing 2/src/main/java/com/iAnalyze/data/processing/BigDataProcessing/BigDataProcessing/controller/BatchController.java
@@ -7,10 +7,13 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/batch")
 public class BatchController {
     @Autowired
     JobLauncher jobLauncher;
@@ -18,16 +21,18 @@ public class BatchController {
     @Autowired
     Job job;
 
-    @RequestMapping("/runjob")
-    public String handle() throws Exception {
+    @RequestMapping("/run")
+    public ResponseEntity<?> handle() throws Exception {
         Logger logger = LoggerFactory.getLogger(this.getClass());
         try {
-            JobParameters jobParameters = new JobParametersBuilder().addLong("Time:", System.currentTimeMillis()).toJobParameters();
+            JobParameters jobParameters = new JobParametersBuilder().addLong("Time:",
+                    System.currentTimeMillis()).toJobParameters();
             jobLauncher.run(job, jobParameters);
+            return ResponseEntity.status(HttpStatus.OK).body("Job is currently running");
 
         } catch (Exception ex) {
             logger.info(ex.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex);
         }
-        return "Batch job is complete";
     }
 }
